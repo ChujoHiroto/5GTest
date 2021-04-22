181830883687169652002726584e3c9a19d494b6
Hiroto Chujo <hiroto@irossoftware.com>
2021-04-23 08:13:56 +0900

    log

--- icmp.sh
+++ icmp.sh
@@ -2,11 +2,11 @@
 echo "Network Test Result for" $1
 date
 echo $2
-echo "External IP : 211.9.56.36"
+echo "External IP : "
 echo "Network Type : "$3
 echo "ICMP Test :"
 ping $1 -c 10
 echo "========================="
 echo Traceroute :
-traceroute -I -m 25 -n $1
+traceroute -m 25 -n $1
 echo "========================="
